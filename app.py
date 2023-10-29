761ae21bccd9f727c2aabbbcce61f2c8d988326c
its_fatemeh_mh <fatemeh.mirhoseini77@gmail.com>
2023-10-29 23:33:50 +0330

    fix pdf to text

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfReader
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader, TextLoader
 import os 
 import streamlit as st 
 from langchain.prompts import PromptTemplate
@@ -8,7 +8,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import LlamaCppEmbeddings
 from langchain.chains import LLMChain
 from langchain.vectorstores import Chroma 
- 
+import fitz
 
 #streamlit layout
 st.set_page_config(page_title="DocChat", layout="wide",)
@@ -29,6 +29,21 @@ def save_pdf(doc,file_path):
         f.write(doc.read())
     return  st.success(f"PDF file '{doc.name}' has been saved." )   
 
+def get_pdf_text(pdf):
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+def write_text_file(content, file_path):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(content)
+        return True
+    except Exception as e:
+        print(f"Error occurred while writing the file: {e}")
+        return False
 
 # set prompt template
 prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -51,12 +66,13 @@ st.title("Q & A with Doucuments")
 doc = st.file_uploader("Upload a pdf file", type="pdf", accept_multiple_files=False)
 
 if doc is not None:
-    file_path = "./temp"
-    save_pdf(doc,file_path)
-    loader = PyPDFLoader(file_path=f"./temp/{doc.name}")
-    #pages = loader.load_and_split()
+    file_path = "temp/file.txt"
+    text_data = get_pdf_text(doc)
+    write_text_file(text_data, file_path) 
+    loader = TextLoader(file_path)
+    text = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    texts = text_splitter.split_documents(loader)
+    texts = text_splitter.split_documents(text)
     vectordb = Chroma.from_documents(texts, embeddings)
     st.success("File has been loaded successfully!")
 
